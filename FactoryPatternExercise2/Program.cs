fbc655090c0dad91665d19bb2f4384698627f9af
zach brecht <brechtz32@gmail.com>
2024-10-24 16:58:06 -0500

    first factory pattern excercise 2 commit

--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Which server would you like to use? List, SQL, or Mongo?");
+            
+            string usersDatabase = Console.ReadLine();
+
+            IDataAccess database = DataAccessFactory.GetDataAccessType(usersDatabase);
+
+            List<Product> products = database.LoadData();
+            database.SaveData();
+
+            Console.WriteLine();
+            Console.WriteLine("Here are your products");
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"Product Name: {products[i].Name} | {products[i].Price,0:c}");
+            }
+
+
         }
     }
 }
